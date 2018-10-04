2105e467eaf4e6558785cb6606a00585b1bf32c7
Damien ONeal <doneal@chef.io>
2018-10-03 23:05:40 -0500

    added CPU feature

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a static webpage to level up on golang and the habitat.sh project (An OS
 Version History:
 
 1.0.0 -- inital commit; static webpage that prints "Hello, World!" on port 1911
+
+1.1.0 -- CPU Feature added. It will list all your cores (Logical + physical cores).
+
+For example: If your system has a 2 physical cores, and 2 logical cores due to Hyper-Threading (Intel chips)/Simultaneous MuliThread (SMT - AMD CHips), you will have 4 total cores.
--- goworld.go
+++ goworld.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 )
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!") // Format what's specificed: ResponseWriter and Request
+	//var cpu int = runtime.NumCPU()
+	fmt.Fprintf(w, "Hello, World! Here's some info about your system: \n \n") // Format what's specificed: ResponseWriter and Request
+	fmt.Fprintf(w, "CPU:", runtime.NumCPU())
 }
 
 func main() {
 	http.HandleFunc("/", index_handler)
 	http.ListenAndServe(":1911", nil) // ListenAndServe needs server functoinality in the 2nd parameter. Not needed here, so nill is used
-
 }
